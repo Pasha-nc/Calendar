c92b5256bd90d99e5e9beb55431806baa2ea9530
Pasha-nc <pasha.nc@gmail.com>
2021-12-24 13:48:18 +0200

    feat_recordsStatusSelect

--- WebApplication1/WebApplication1/wwwroot/Scripts/FillRecords.js
+++ WebApplication1/WebApplication1/wwwroot/Scripts/FillRecords.js
@@ -1,12 +1,30 @@
-﻿const addRecordRow = function () {
+﻿const addRecordRow = function (x) {
     const recordsAddRow = document.getElementById('recordsAddRow');
 
     let recRow = document.createElement("tr");
-    recRow.innerHTML = "<td class='recordsCell'>iD</td><td class='recordsCell' colspan='5'>Record</td><td class='recordsCell'>delRec</td>";
+    recRow.setAttribute("id", "recordRow" + x.toString());
+    recRow.innerHTML = "<td class='recordsCell' id='idCell" + x.toString() + "'>iD</td><td class='recordsCell' colspan='8' id='recordCell" + x.toString() + "'>Record</td><td class='recordsCellStatus' colspan = '2' id='statusCell" + x.toString() + "'>Status</td><td class='recordsCell' id='delRecCell" + x.toString() + "'>del</td>";
 
     recordsAddRow.parentNode.insertBefore(recRow, recordsAddRow);
 }
 
-addRecordRow();
-addRecordRow();
-addRecordRow();
+addRecordRow(0);
+addRecordRow(1);
+addRecordRow(2);
+
+const delRecord = function (y) {
+    console.log(document.getElementById("idCell" + y.toString()).innerHTML);    
+}
+
+delRecord(1);
+
+const addStatusList = function () {
+    const myCells = document.getElementsByClassName('recordsCellStatus');
+
+    for (var i = 0; i < myCells.length; i++) {
+        console.log(myCells.item(i));
+        myCells.item(i).innerHTML = "<select class='selectStatus'><option>Status1</option><option>Status2</option><option>Status3</option></select>";
+    }
+}
+
+addStatusList();
