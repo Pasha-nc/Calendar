f93c112b2c06ca72a4c5c8806997d426d701f8d2
Pasha-nc <pasha.nc@gmail.com>
2021-12-24 11:56:19 +0200

    feat_recordTable

---
+++ WebApplication1/WebApplication1/wwwroot/Scripts/FillRecords.js
@@ -0,0 +1,12 @@
+﻿const addRecordRow = function () {
+    const recordsAddRow = document.getElementById('recordsAddRow');
+
+    let recRow = document.createElement("tr");
+    recRow.innerHTML = "<td class='recordsCell'>iD</td><td class='recordsCell' colspan='5'>Record</td><td class='recordsCell'>delRec</td>";
+
+    recordsAddRow.parentNode.insertBefore(recRow, recordsAddRow);
+}
+
+addRecordRow();
+addRecordRow();
+addRecordRow();
